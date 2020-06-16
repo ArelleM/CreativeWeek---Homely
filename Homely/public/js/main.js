ca2a84ff112affbf2c3e851090acdd9e5e67f868
Arelle <clerc.angelique@gmail.com>
2020-06-16 16:54:13 +0200

    Navbar finie

---
+++ Homely/public/js/main.js
@@ -0,0 +1,15 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function Dropdown() {
+    document.getElementById("UserDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var UserDropdown = document.getElementById("UserDropdown");
+        if (UserDropdown.classList.contains('show')) {
+            UserDropdown.classList.remove('show');
+        }
+    }
+}
