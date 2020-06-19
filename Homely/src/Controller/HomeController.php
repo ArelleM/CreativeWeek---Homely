5b74c31897ec7053f7fdf9b117d65371c619cf9e
Arelle <clerc.angelique@gmail.com>
2020-06-19 15:23:59 +0200

    Projet J5

--- Homely/src/Controller/HomeController.php
+++ Homely/src/Controller/HomeController.php
@@ -41,4 +41,26 @@ class HomeController extends AbstractController
         return $this->render('pages/location.html.twig');
 
     }
+
+    /**
+     * @Route("/apropos", name="apropos")
+     * @return Response
+     *
+     */
+    public function apropos(): Response
+    {
+        return $this->render('pages/apropos.html.twig');
+
+    }
+    /**
+     * @Route("/acheter", name="acheter")
+     * @return Response
+     *
+     */
+    public function acheter(): Response
+    {
+        return $this->render('pages/acheter.html.twig');
+
+    }
+
 }
